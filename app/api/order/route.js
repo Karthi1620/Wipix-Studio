f23f5270bb2d97f5e6840a9305808320f769a7be
LUCIFER ANTONY <lucifer@LUCIFER.LUCIFER>
2025-02-23 20:49:57 +0530

    Fixed missing modules for MongoDB

--- app/api/order/route.js
+++ app/api/order/route.js
@@ -1,5 +1,6 @@
-import { connectToDatabase } from "@/lib/mongodb";
-import Order from "@/models/order";
+import { connectDB } from "@/app/lib/mongodb";
+import Order from "@/app/models/order";
+
 
 export async function POST(req) {
     try {
